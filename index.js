29478a02dfd80803d9f3f74d682a041f1a4a56b1
Fatemin Supria <fateminsupria.me2016@gmail.com>
2021-04-02 19:55:07 +0600

    add

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgh2s.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const app = express()
 
-// middle wire
+// middle wire start
 app.use(bodyParser.json());
 app.use(cors()); 
 
